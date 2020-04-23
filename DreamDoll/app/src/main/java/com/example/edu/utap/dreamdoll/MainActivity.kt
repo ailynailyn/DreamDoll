f8acf71124e849ec177146da668b6ab67827076d
Lexi Cepak <CepakA@advisory.com>
2020-04-23 03:32:38 -0500

    merged everything + added splash screen

--- DreamDoll/app/src/main/java/com/example/edu/utap/dreamdoll/MainActivity.kt
+++ DreamDoll/app/src/main/java/com/example/edu/utap/dreamdoll/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -194,6 +195,7 @@ class MainActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mAuth = FirebaseAuth.getInstance()
         var curUser = mAuth.currentUser
 
